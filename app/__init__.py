e9406461f2bea163104d1682a0b6696c633c6a3f
Abhinav Suri <suriabhinav1997@gmail.com>
2015-09-20 13:59:39 -0400

    new database

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from config import config
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(app.config.from_object(config['development']))
 
-from app import views
+db = SQLAlchemy(app)
+
+from app import views, models
