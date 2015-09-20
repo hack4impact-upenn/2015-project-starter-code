39f14912d8276887311ca2c503813cce52782bb0
Abhinav Suri <suriabhinav1997@gmail.com>
2015-09-20 13:23:08 -0400

    Added Health

--- app/views.py
+++ app/views.py
@@ -3,4 +3,9 @@ from app import app
 
 @app.route('/', methods=['GET'])
 def index():
-    return 'Hello world!'
+    return 'Hello world!', 200
+
+
+@app.route('/health',methods=['GET'])
+def health():
+	return 'OK ;)', 200
