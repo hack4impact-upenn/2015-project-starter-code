6bd1598cdfbccdffc2d0dd3e56b27fda4646f6cd
Hunter Lightman <git.huntrr@gmail.com>
2015-09-20 14:10:51 -0400

    Added db

---
+++ app/models.py
@@ -0,0 +1,30 @@
+from . import db
+from datetime import datetime
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def is_cool(self):
+        return (self.username == 'hunterl')
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    body = db.Column(db.Text)
+    pub_date = db.Column(db.DateTime, default=datetime.utcnow())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post %r>' % self.title
